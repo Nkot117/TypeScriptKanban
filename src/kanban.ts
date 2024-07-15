0bce2e13c0d1eb5f4ca8157c031720d826d7ae68
Nkot117 <kumakura117@gmail.com>
2024-07-15 22:32:35 +0900

    リファクタリング

--- src/kanban.ts
+++ src/kanban.ts
@@ -1,3 +1,40 @@
+abstract class UIComponent<T extends HTMLElement> {
+    templateElement: HTMLTemplateElement;
+    element: T;
+
+    constructor(selector: string) {
+        this.templateElement = document.querySelector(selector) as HTMLTemplateElement;
+
+        const clone = this.templateElement.content.cloneNode(true) as DocumentFragment;
+
+        this.element = clone.firstElementChild as T;
+    }
+
+    mount(selector: string) {
+        const targetElement = document.querySelector(selector);
+        targetElement?.insertAdjacentElement("beforeend", this.element);
+    }
+
+    abstract setup(): void
+
+    abstract setup(): void;
+}
+
+interface ClickableElement {
+    element: HTMLElement;
+    clickHandler(event: MouseEvent): void;
+    bindEvents(): void;
+}
+
+interface Task {
+    title: string;
+    description?: string;
+}
+
+// タスクの状態を定義
+const TASK_STATUS = ["todo", "working", "done"] as const;
+type TaskStatus = (typeof TASK_STATUS)[number];
+
 class TaskForm {
     // 入力フォーム
     element: HTMLFormElement;
@@ -19,7 +56,7 @@ class TaskForm {
         event.preventDefault();
 
         const task = this.createTask();
-        const item = new TaskItem("#task-item-template", task);
+        const item = new TaskItem(task);
         item.mount("#todo");
         this.clearInputs();
     }
@@ -42,36 +79,9 @@ class TaskForm {
     }
 }
 
-interface Task {
-    title: string;
-    description?: string;
-}
-
-class TaskList {
-    // テンプレート要素
-    templateElement: HTMLTemplateElement;
-    // テンプレートからコピーした要素
-    element: HTMLDivElement;
-    // タスクの状態
-    private taskStatus;
-    constructor(templateId: string, _taskStatus: string) {
-        // テンプレート要素を取得
-        this.templateElement = document.querySelector(templateId) as HTMLTemplateElement;
-
-        // テンプレート要素のコピーを作成
-        const templateClone = this.templateElement!.content.cloneNode(true) as DocumentFragment;
-
-        // コピーした要素の最初の子要素を取得
-        // <div class="tasks">
-        //   <h2 class="list-title"></h2>
-        //   <ul id="list">
-        //     <!-- タスクを追加する -->
-        //   </ul>
-        // </div>
-        this.element = templateClone.firstElementChild as HTMLDivElement;
-
-        this.taskStatus = _taskStatus;
-
+class TaskList extends UIComponent<HTMLDivElement> {
+    constructor(private taskStatus: TaskStatus) {
+        super("#task-list-template");
         this.setup();
     }
 
@@ -82,43 +92,17 @@ class TaskList {
         // id属性を設定
         this.element.querySelector("ul")!.id = `${this.taskStatus}`;
     }
-
-    mount(selector: string) {
-        const targetElement = document.querySelector(selector);
-        targetElement?.insertAdjacentElement("beforeend", this.element);
-    }
 }
 
-// タスクの状態を定義
-const TASK_STATUS = ["todo", "working", "done"] as const;
-type TaskStatus = (typeof TASK_STATUS)[number];
-
-new TaskForm();
-
-TASK_STATUS.forEach((status) => {
-    const list = new TaskList("#task-list-template", status);
-    list.mount("#container");
-});
-
-class TaskItem {
-    templateElement: HTMLTemplateElement;
-    element: HTMLElement;
+class TaskItem extends UIComponent<HTMLElement> implements ClickableElement {
     task: Task;
 
-    constructor(templateId: string, _task: Task) {
-        this.templateElement = document.querySelector(templateId) as HTMLTemplateElement;
-        const templateClone = this.templateElement.content.cloneNode(true) as DocumentFragment;
-        this.element = templateClone.firstElementChild as HTMLElement;
+    constructor(_task: Task) {
+        super("#task-item-template");
+
         this.task = _task;
-
         this.setup();
-        this.bindEvents();        
-    }
-
-    mount(selector: string) {
-        const targetElement = document.querySelector(selector);
-        console.log(targetElement);
-        targetElement?.insertAdjacentElement("beforeend", this.element);
+        this.bindEvents();
     }
 
     setup() {
@@ -126,20 +110,20 @@ class TaskItem {
         this.element.querySelector("p")!.textContent = `${this.task.description}`;
     }
 
-    clickHandler(){
+    clickHandler() {
         console.log("click");
-        if(!this.element.parentElement)  return;
+        if (!this.element.parentElement) return;
 
         const currentListId = this.element.parentElement.id as TaskStatus;
         const taskStatusIdx = TASK_STATUS.indexOf(currentListId);
 
-        if(taskStatusIdx == -1){
+        if (taskStatusIdx == -1) {
             throw new Error("invalid task status");
         }
 
         const nextListId = TASK_STATUS[taskStatusIdx + 1] as TaskStatus;
 
-        if(nextListId) {
+        if (nextListId) {
             const newstListElement = document.querySelector(`#${nextListId}`) as HTMLUListElement;
             newstListElement.appendChild(this.element);
             return;
@@ -152,3 +136,10 @@ class TaskItem {
         this.element.addEventListener("click", this.clickHandler.bind(this));
     }
 }
+
+new TaskForm();
+
+TASK_STATUS.forEach((status) => {
+    const list = new TaskList(status);
+    list.mount("#container");
+});
