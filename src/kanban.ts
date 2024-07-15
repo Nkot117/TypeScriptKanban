d5c8bcf448233c78a0b9c358c7d48c0970c2f8c7
Nkot117 <kumakura117@gmail.com>
2024-07-15 18:39:52 +0900

    フォームの入力内容を受け取るクラスを作成

--- src/kanban.ts
+++ src/kanban.ts
@@ -1,1 +1,31 @@
-console.log("Hello, Kanban!");
+class TaskForm {
+    // 入力フォーム
+    element: HTMLFormElement;
+    // タイトル入力欄
+    titleInputElement: HTMLInputElement;
+    // 詳細入力欄
+    descriptionInputElement: HTMLInputElement;
+
+    constructor() {
+        this.element = document.querySelector("#task-form") as HTMLFormElement;
+        this.titleInputElement = document.querySelector("#form-title") as HTMLInputElement;
+        this.descriptionInputElement = document.querySelector("#form-description") as HTMLInputElement;
+
+        // イベントリスナーの登録
+        this.bindEvents()
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        
+        console.log(this.titleInputElement.value);
+        console.log(this.descriptionInputElement.value);
+    }
+
+    private bindEvents() {
+        // bindメソッドを使って、submitHandlerメソッド内のthisがTaskFormクラスのインスタンスを指すようにする
+        this.element.addEventListener("submit", this.submitHandler.bind(this))  ;
+    }
+}
+
+new TaskForm();
