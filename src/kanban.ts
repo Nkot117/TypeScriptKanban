2337a3064561f7917e81206940ae6323250b7520
Nkot117 <kumakura117@gmail.com>
2024-07-15 22:00:20 +0900

    タスクリストを追加

--- src/kanban.ts
+++ src/kanban.ts
@@ -17,7 +17,7 @@ class TaskForm {
 
     private submitHandler(event: Event) {
         event.preventDefault();
-        
+
         const task = this.createTask();
         console.log(task);
 
@@ -47,4 +47,55 @@ interface Task {
     description?: string;
 }
 
+class TaskList {
+    // テンプレート要素
+    templateElement: HTMLTemplateElement;
+    // テンプレートからコピーした要素
+    element: HTMLDivElement;
+    // タスクの状態
+    private taskStatus;
+    constructor(templateId: string, _taskStatus: string) {
+        // テンプレート要素を取得
+        this.templateElement = document.querySelector(templateId) as HTMLTemplateElement;
+
+        // テンプレート要素のコピーを作成
+        const templateClone = this.templateElement!.content.cloneNode(true) as DocumentFragment;
+
+        // コピーした要素の最初の子要素を取得
+        // <div class="tasks">
+        //   <h2 class="list-title"></h2>
+        //   <ul id="list">
+        //     <!-- タスクを追加する -->
+        //   </ul>
+        // </div>
+        this.element = templateClone.firstElementChild as HTMLDivElement;
+
+        this.taskStatus = _taskStatus;
+
+        this.setup();
+    }
+
+    setup() {
+        // タスクの状態を設定
+        this.element.querySelector("h2")!.textContent = this.taskStatus;
+
+        // id属性を設定
+        this.element.querySelector("ul")!.id = `$(this.taskStatus)`;
+    }
+
+    mount(selector: string) {
+        const targetElement = document.querySelector(selector);
+        targetElement?.insertAdjacentElement("beforeend", this.element);
+    }
+}
+
+// タスクの状態を定義
+const TASK_STATUS = ["todo", "working", "done"] as const;
+type TaskStatus = (typeof TASK_STATUS)[number];
+
 new TaskForm();
+
+TASK_STATUS.forEach((status) => {
+    const list = new TaskList("#task-list-template", status);
+    list.mount("#container");
+});
